af9ee73e0f41c3cda041aef04a1151444f262747
Jevgenijs Kozlovs <jevgenijs_kozlovs@epam.com>
2023-06-27 17:27:31 +0300

    init project

---
+++ iosvideo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iosvideo
+//
+//  Created by Jevgenijs Kozlovs on 27/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
